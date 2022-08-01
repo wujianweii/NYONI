5fe23d745f00830b25cfbcf71a00a6da279eb5a3
wujianwei <1121071856@qq.com>
2022-08-01 17:50:32 +0800

    build fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import home from "../views/Home.vue";
 import nyoni from "../views/About/Nyoni.vue";
 import jiuling from "../views/About/Jiuling.vue";
@@ -64,7 +64,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(process.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 });
 
